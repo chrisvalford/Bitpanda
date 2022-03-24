a8c00b4c99db6c686d7f244e84b6832025891d55
Christopher Alford <chrisvalford@me.com>
2022-03-24 16:29:15 +0100

    Basic wallet view completed

---
+++ Bitpanda/Scenes/WalletsView/FiatWalletTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FiatWalletTableViewCell.swift
+//  Bitpanda
+//
+//  Created by Christopher Alford on 24/3/22.
+//  Copyright © 2022 Digital+Marine. All rights reserved.
+//
+
+import UIKit
+
+class FiatWalletTableViewCell: UITableViewCell {
+    
+    var viewModel: WalletView?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func layout(){}
+
+}
--- Bitpanda/Scenes/WalletsView/WalletsViewController.swift
+++ Bitpanda/Scenes/WalletsView/WalletsViewController.swift
@@ -9,17 +9,68 @@
 import UIKit
 
 class WalletsViewController: UIViewController {
+    
+    let walletCellId = "WalletCell"
+    let fiatWalletCellId = "FiatWalletCell"
+    
+    private let viewModel = WalletsViewModel()
+    
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.dataSource = self
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.register(WalletTableViewCell.self, forCellReuseIdentifier: walletCellId)
+        tv.register(FiatWalletTableViewCell.self, forCellReuseIdentifier: fiatWalletCellId)
+        return tv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.addSubview(tableView)
+        addContsraints()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    private func addContsraints() {
+        tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+        tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 8).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
 }
 
+extension WalletsViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.walletData.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.walletData[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: fiatWalletCellId, for: indexPath) as! FiatWalletTableViewCell
+            cell.viewModel = viewModel.walletData[indexPath.section][indexPath.row]
+            cell.layout()
+            cell.backgroundColor = .blue
+            return cell
+        default:
+            let cell = tableView.dequeueReusableCell(withIdentifier: walletCellId, for: indexPath) as! WalletTableViewCell
+            cell.viewModel = viewModel.walletData[indexPath.section][indexPath.row]
+            cell.layout()
+            cell.backgroundColor = indexPath.section == 0 ? .green : .yellow
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return viewModel.headerData[section]
+    }
+}
