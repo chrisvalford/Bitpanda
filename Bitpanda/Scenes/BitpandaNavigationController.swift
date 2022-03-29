99f02442e09a8cd9498242b4de2a8012aa1c350b
Christopher Alford <chrisvalford@me.com>
2022-03-29 10:04:08 +0200

    Post stash addition

---
+++ Bitpanda/Scenes/BitpandaNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  BitpandaNavigationController.swift
+//  Bitpanda
+//
+//  Created by Christopher Alford on 28/3/22.
+//  Copyright © 2022 Digital+Marine. All rights reserved.
+//
+
+import UIKit
+
+class BitpandaNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
